d3086425e5f22d152ff53dc4920d844b800b6470
Barry <bxie@apartnership.com>
2021-10-12 23:16:27 -0400

    finished

--- calculator-test.js
+++ calculator-test.js
@@ -1,11 +1,11 @@
 
 it('should calculate the monthly rate correctly', function () {
-  // ...
+  expect(calculateMonthlyPayment({amount: 10000, years: 10, rate: 4.5})).toEqual("103.64")
 });
 
 
 it("should return a result with 2 decimal places", function() {
-  // ..
+  expect(calculateMonthlyPayment({amount: 165000, years: 30, rate: 3.8})).toEqual("768.83")
 });
 
 /// etc
